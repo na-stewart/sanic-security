673b5898dc3dc7278d7f2a367ca40d6f82cea91c
sunset <aidanstewart@sunsetdeveloper.com>
2020-11-18 13:59:48 -0500

    changes

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("requirements.txt", "r") as rq:
 
 setup(
     name='amyrose',
-    version='0.2.7',
+    version='0.3.0',
     packages=setuptools.find_packages(),
     url='https://github.com/sunset-developer/Amy-Rose',
     license='GNU General Public License v3.0',
