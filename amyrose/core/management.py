fa9018b2a6521fe45dcdae473451a6fb1d1859e1
sunset <aidanstewart@sunsetdeveloper.com>
2020-12-13 19:48:35 -0500

    docs

--- amyrose/core/management.py
+++ amyrose/core/management.py
@@ -81,7 +81,7 @@ async def get_client(request: Request):
 
 async def get_accounts():
     """
-    Retrieves all accounts in database.
+    Retrieves all accounts.
 
     :return: accounts
     """
