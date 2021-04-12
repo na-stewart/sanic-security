4295855b6b380e040dd8454ee804c4322140fe62
sunset <aidanstewart@sunsetdeveloper.com>
2021-04-12 10:28:46 -0400

    setup

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("requirements.txt", "r") as rq:
 
 setup(
     name='sanic-security',
-    version='0.8.2.0',
+    version='0.8.2.1',
     packages=setuptools.find_packages(),
     url='https://github.com/sunset-developer/sanic-security',
     license='GNU General Public License v3.0',
