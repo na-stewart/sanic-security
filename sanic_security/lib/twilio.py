9186756d9abcf0cbac177d7adc660aa9cbc41828
sunset <aidanstewart@sunsetdeveloper.com>
2021-05-03 11:15:16 -0400

    aiohttp to httpx

--- sanic_security/lib/twilio.py
+++ sanic_security/lib/twilio.py
@@ -1,4 +1,4 @@
-import aiohttp
+import httpx
 
 from sanic_security.core.utils import config
 
@@ -15,8 +15,8 @@ async def send_sms(to, msg):
     auth_token = config['TWILIO']['token']
     from_num = '+' + config['TWILIO']['from']
     if account_sid and auth_token and from_num:
-        auth = aiohttp.BasicAuth(login=account_sid, password=auth_token)
-        async with aiohttp.ClientSession(auth=auth) as session:
+        auth = httpx.BasicAuth(username=account_sid, password=auth_token)
+        async with httpx.AsyncClient(auth=auth) as session:
             await session.post(f'https://api.twilio.com/2010-04-01/Accounts/{account_sid}/Messages.json',
                                data={'From': from_num, 'To': '+' + to, 'Body': msg})
     else:
