b9feeca19b46efefed567b5a066e0ffe6c76126f
sunset <aidanstewart@sunsetdeveloper.com>
2021-05-03 21:07:23 -0400

    hash_password documentation fix

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("requirements.txt", "r") as rq:
 
 setup(
     name='sanic-security',
-    version='0.9.3',
+    version='0.9.4',
     packages=setuptools.find_packages(),
     url='https://github.com/sunset-developer/sanic-security',
     license='GNU General Public License v3.0',
