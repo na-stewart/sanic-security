2327302a74dba299fbcd38e8744ae0e461a5127d
sunset <aidanstewart@sunsetdeveloper.com>
2021-05-10 13:48:54 -0400

    tortoise configuration fix.

--- sanic_security/lib/tortoise.py
+++ sanic_security/lib/tortoise.py
@@ -12,19 +12,19 @@ def initialize_tortoise(app: Sanic):
         app (Sanic): Sanic Framework app.
     """
 
-    @app.listener("before_server_start")
+    @app.listener('before_server_start')
     async def init_orm(app, loop):
-        username = config["TORTOISE"]["username"]
-        password = config["TORTOISE"]["password"]
-        endpoint = config["TORTOISE"]["endpoint"]
-        schema = config["TORTOISE"]["schema"]
-        engine = config["TORTOISE"]["engine"]
-        models = config["TORTOISE"]["models"].replace("\\s", "").split(",")
-        url = engine + "://{0}:{1}@{2}/{3}".format(username, password, endpoint, schema)
-        await Tortoise.init(db_url=url, modules={"models": models})
-        if config["TORTOISE"]["generate"] == "true":
+        username = config['TORTOISE']['username']
+        password = config['TORTOISE']['password']
+        endpoint = config['TORTOISE']['endpoint']
+        schema = config['TORTOISE']['schema']
+        engine = config['TORTOISE']['engine']
+        models = config['TORTOISE']['models'].replace(' ', '').split(',')
+        url = engine + '://{0}:{1}@{2}/{3}'.format(username, password, endpoint, schema)
+        await Tortoise.init(db_url=url, modules={'models': models})
+        if config['TORTOISE']['generate'] == 'true':
             await Tortoise.generate_schemas()
 
-    @app.listener("after_server_stop")
+    @app.listener('after_server_stop')
     async def close_orm(app, loop):
         await Tortoise.close_connections()
