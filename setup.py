0022331bbb7a9b499e39a7b8bb1590208c8845d9
sunset <aidanstewart@sunsetdeveloper.com>
2021-06-11 11:48:11 -0400

    setup.py changes

--- setup.py
+++ setup.py
@@ -4,19 +4,23 @@ from setuptools import setup
 with open("README.md", "r") as rm:
     long_description = rm.read()
 
-with open("requirements.txt", "r") as rq:
-    requirements = [r.strip() for r in rq.readlines()]
-
 setup(
     name='sanic-security',
-    version='0.9.13.3',
+    version='0.9.13.4',
     packages=setuptools.find_packages(),
     url='https://github.com/sunset-developer/sanic-security',
     license='GNU General Public License v3.0',
     author='sunset-developer',
+    install_requires=[
+        'sanic',
+        'tortoise-orm',
+        'aiofiles',
+        'pyjwt',
+        'captcha',
+        'aiosmtplib',
+    ],
     author_email='aidanstewart@sunsetdeveloper.com',
     description='A powerful, simple, and async security library for Sanic.',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    install_requires=requirements
 )
