e5a488d336496a993b729435f33079ff4e2945f2
sunset <aidanstewart@sunsetdeveloper.com>
2021-06-27 16:37:55 -0400

    Sanic dependency update

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ with open("README.md", "r") as rm:
 
 setup(
     name="sanic-security",
-    version="0.10.0",
+    version="0.10.0.2",
     packages=setuptools.find_packages(),
     url="https://github.com/sunset-developer/sanic-security",
     license="GNU General Public License v3.0",
     author="sunset-developer",
     install_requires=[
-        "sanic==21.3.4",
+        "sanic==21.6.0",
         "tortoise-orm==0.17.4",
         "aiofiles==0.7.0",
         "pyjwt==2.1.0",
