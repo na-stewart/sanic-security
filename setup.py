1763851b929af63fe9ee4226cff2de6fc1ed1e6d
sunset <aidanstewart@sunsetdeveloper.com>
2021-07-11 19:39:05 -0400

    dependency versioning improvements

--- setup.py
+++ setup.py
@@ -6,19 +6,19 @@ with open("README.md", "r") as rm:
 
 setup(
     name="sanic-security",
-    version="0.10.3",
+    version="0.10.3.1",
     packages=setuptools.find_packages(),
     url="https://github.com/sunset-developer/sanic-security",
     license="GNU General Public License v3.0",
     author="sunset-developer",
     install_requires=[
-        "sanic==21.6.0",
-        "tortoise-orm==0.17.5",
-        "aiofiles==0.7.0",
-        "pyjwt==2.1.0",
-        "captcha==0.3",
-        "aiosmtplib==1.1.6",
-        "httpx==0.18.2"
+        "sanic>=21.3.0",
+        "tortoise-orm>=0.17.0",
+        "aiofiles",
+        "pyjwt",
+        "captcha",
+        "aiosmtplib",
+        "httpx"
     ],
     author_email="aidanstewart@sunsetdeveloper.com",
     description="A powerful, simple, and async security library for Sanic.",
