7faa37d3fedadce3b36f72ee2bbfee4209cfafaf
sunset <aidanstewart@sunsetdeveloper.com>
2021-08-28 11:35:26 +0900

    allow_unverified removed, account validation checked removed from requesting verification, validation documentation

--- sanic_security/test/server.py
+++ sanic_security/test/server.py
@@ -38,7 +38,7 @@ async def on_register(request):
     Register an account with an email, username, and password. Once the account is created successfully, a two-step session is requested and the code is provided in the response.
     """
     account = await register(request)
-    two_step_session = await request_two_step_verification(request, account, True)
+    two_step_session = await request_two_step_verification(request, account)
     await two_step_session.email_code()
     response = json("Registration successful!", two_step_session.code)
     two_step_session.encode(response, False)
