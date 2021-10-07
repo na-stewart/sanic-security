92b1e8b0fb13d5d593c0e4e91e3b15ebd3dcdd60
Sunset Dev <aidanstewart@sunsetdeveloper.com>
2021-10-07 15:11:29 -0400

    License notice, setup versioning.

--- sanic_security/authentication.py
+++ sanic_security/authentication.py
@@ -12,6 +12,23 @@ from sanic_security.exceptions import (
 from sanic_security.models import Account, SessionFactory, AuthenticationSession
 from sanic_security.utils import hash_password, get_ip
 
+"""
+Copyright (C) 2021 Aidan Stewart
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>
+"""
+
 session_factory = SessionFactory()
 
 
--- setup.py
+++ setup.py
@@ -1,6 +1,23 @@
 import setuptools
 from setuptools import setup
 
+"""
+Copyright (C) 2021 Aidan Stewart
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>
+"""
+
 with open("README.md", "r") as rm:
     long_description = rm.read()
 
@@ -13,7 +30,7 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     license="GNU General Public License v3.0",
-    version="1.0.0.0",
+    version="1.0.0.1",
     packages=setuptools.find_packages(),
     python_requires=">=3.6",
     install_requires=[
