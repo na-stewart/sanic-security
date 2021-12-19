37d51594b032ecb59535e9b51a05806bae4c0b08
Sunset Dev <aidanstewart@sunsetdeveloper.com>
2021-12-18 21:48:17 -0500

    Semantic versioning.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     license="GNU General Public License v3.0",
-    version="1.3.0.3",
+    version="1.3.1",
     packages=setuptools.find_packages(),
     python_requires=">=3.6",
     install_requires=[
