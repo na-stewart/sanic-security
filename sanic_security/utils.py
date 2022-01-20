9f87db1b92a90629295399894a4786fdcea0096a
Sunset Dev <aidanstewart@sunsetdeveloper.com>
2022-01-20 14:44:36 -0500

    Licensing revision

--- sanic_security/utils.py
+++ sanic_security/utils.py
@@ -4,6 +4,25 @@ from sanic.request import Request
 from sanic.response import json as sanic_json, HTTPResponse
 
 
+"""
+An effective, simple, and async security library for the Sanic framework.
+Copyright (C) 2021 Aidan Stewart
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+
 def get_ip(request: Request) -> str:
     """
     Retrieves ip address from client request.
