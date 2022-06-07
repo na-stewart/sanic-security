f7d24edfd616682ef8e498b1462c6dbdcb4e4533
Rob Dailey <rob@wargam.es>
2022-06-06 19:03:24 -0700

    Migrated to `pytest` and `sanic-testing`

--- tests/test_02_registration.py
+++ tests/test_02_registration.py
@@ -37,8 +37,8 @@ class TestRegistration:
         verified: bool,
         phone: str = None,
     ):
-        registration_response = client.post(
-            "http://127.0.0.1:8000/api/test/auth/register",
+        registration_request, registration_response = client.post(
+            "/api/test/auth/register",
             data={
                 "username": username,
                 "email": email,
@@ -48,7 +48,7 @@ class TestRegistration:
                 "phone": phone,
             },
         )
-        return registration_response
+        return registration_request, registration_response
 
     def test_registration(self, app: Sanic):
         """
@@ -56,36 +56,36 @@ class TestRegistration:
         """
         _client = ReusableClient(app, host='127.0.0.1', port='8000')
         with _client:
-            registration_response = self.register(
+            registration_request, registration_response = self.register(
                 _client, "emailpass@register.com", "emailpass", False, True
             )
-            assert registration_response.status_code == 200, registration_response.text
+            assert registration_response.status == 200, registration_response.text
 
-    def test_invalid_registration(self):
+    def test_invalid_registration(self, app: Sanic):
         """
         Registration with an intentionally invalid email, username, and phone.
         """
         _client = ReusableClient(app, host='127.0.0.1', port='8000')
         with _client:
-            invalid_email_registration_response = self.register(
+            invalid_email_registration_request, invalid_email_registration_response = self.register(
                 _client, "invalidregister.com", "invalid", False, True
             )
             assert (
-                invalid_email_registration_response.status_code == 400
+                invalid_email_registration_response.status == 400
             ), invalid_email_registration_response.text
-            invalid_phone_registration_response = self.register(
+            invalid_phone_registration_request, invalid_phone_registration_response = self.register(
                 _client, "invalidnum@register.com", "invalidnum", False, True, phone="218183186"
             )
             assert (
-                invalid_phone_registration_response.status_code == 400
+                invalid_phone_registration_response.status == 400
             ), invalid_phone_registration_response.text
-            invalid_username_registration_response = self.register(
+            invalid_username_registration_request, invalid_username_registration_response = self.register(
                 _client, "invaliduser@register.com", "_inVal!d_", False, True
             )
             assert (
-                invalid_username_registration_response.status_code == 400
+                invalid_username_registration_response.status == 400
             ), invalid_username_registration_response.text
-            too_many_characters_registration_response = self.register(
+            too_many_characters_registration_request, too_many_characters_registration_response = self.register(
                 _client,
                 "toolonguser@register.com",
                 "thisusernameistoolongtoberegisteredwith",
@@ -93,46 +93,52 @@ class TestRegistration:
                 True,
             )
             assert (
-                too_many_characters_registration_response.status_code == 400
+                too_many_characters_registration_response.status == 400
             ), too_many_characters_registration_response.text
     
-    def test_registration_disabled(self):
+    def test_registration_disabled(self, app: Sanic):
         """
         Registration and login with a disabled account.
         """
-        registration_response = self.register(
-            "disabled@register.com", "disabled", True, True
-        )
-        assert registration_response.status_code == 200, registration_response.text
-        login_response = self.client.post(
-            "http://127.0.0.1:8000/api/test/auth/login",
-            auth=("disabled@register.com", "testtest"),
-        )
-        assert "DisabledError" in login_response.text, login_response.text
+        _client = ReusableClient(app, host='127.0.0.1', port='8000')
+        with _client:
+            registration_request, registration_response = self.register(
+                _client, "disabled@register.com", "disabled", True, True
+            )
+            assert registration_response.status == 200, registration_response.text
+            login_request, login_response = _client.post(
+                "/api/test/auth/login",
+                auth=("disabled@register.com", "testtest"),
+            )
+            assert "DisabledError" in login_response.text, login_response.text
 
-    def test_registration_unverified(self):
+    def test_registration_unverified(self, app: Sanic):
         """
         Registration and login with an unverified account.
         """
-        registration_response = self.register(
-            "unverified@register.com", "unverified", False, False
-        )
-        assert registration_response.status_code == 200, registration_response.text
-        login_response = self.client.post(
-            "http://127.0.0.1:8000/api/test/auth/login",
-            auth=("unverified@register.com", "testtest"),
-        )
-        assert "UnverifiedError" in login_response.text, login_response.text
+        _client = ReusableClient(app, host='127.0.0.1', port='8000')
+        with _client:
+            registration_request, registration_response = self.register(
+                _client, "unverified@register.com", "unverified", False, False
+            )
+            assert registration_response.status == 200, registration_response.text
+            login_request, login_response = _client.post(
+                "/api/test/auth/login",
+                auth=("unverified@register.com", "testtest"),
+            )
+            assert "UnverifiedError" in login_response.text, login_response.text
 
-    def test_registration_unverified_disabled(self):
+    def test_registration_unverified_disabled(self, app: Sanic):
         """
         Registration and login with an unverified and disabled account.
         """
-        registration_response = self.register(
-            "unverified_disabled@register.com", "unverified_disabled", True, False
-        )
-        assert registration_response.status_code == 200, registration_response.text
-        login_response = self.client.post(
-            "http://127.0.0.1:8000/api/test/auth/login",
-            auth=("unverified_disabled@register.com", "testtest"),
-        )
+        _client = ReusableClient(app, host='127.0.0.1', port='8000')
+        with _client:
+            registration_request, registration_response = self.register(
+                _client, "unverified_disabled@register.com", "unverified_disabled", True, False
+            )
+            assert registration_response.status == 200, registration_response.text
+            login_request, login_response = _client.post(
+                "/api/test/auth/login",
+                auth=("unverified_disabled@register.com", "testtest"),
+            )
