5482e420c651593a7fdecaa50905a11090d93b25
Rob Dailey <rob@wargam.es>
2022-07-06 18:06:46 -0400

    Version bump

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     license="GNU Affero General Public License v3.0",
-    version="2.0.0",
+    version="2.0.0_preview_01",
     packages=setuptools.find_packages(),
     python_requires=">=3.6",
     install_requires=[
