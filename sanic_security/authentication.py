390ff39b0750b80a45e318071a29b1729deadad7
Aidan Stewart <na.stewart365@gmail.com>
2023-07-03 22:31:43 -0400

    Black formatting

--- sanic_security/authentication.py
+++ sanic_security/authentication.py
@@ -350,7 +350,9 @@ def create_initial_admin_account(app: Sanic) -> None:
                 name="Head Admin",
             )
         try:
-            account = await Account.filter(email=security_config.INITIAL_ADMIN_EMAIL).get()
+            account = await Account.filter(
+                email=security_config.INITIAL_ADMIN_EMAIL
+            ).get()
             await account.fetch_related("roles")
             if role not in account.roles:
                 await account.roles.add(role)
