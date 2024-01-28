a8f15bc408634696a2c9c549f5b4e5452791cd5b
na-stewart <na.stewart365@gmail.com>
2024-01-27 21:13:00 -0500

    Decoding fix

--- sanic_security/models.py
+++ sanic_security/models.py
@@ -376,7 +376,7 @@ class Session(BaseModel):
                 raise JWTDecodeError("Session token not provided or expired.", 401)
             else:
                 return jwt.decode(
-                    cookie, security_config.SECRET or security_config.PUBLIC_SECRET
+                    cookie, security_config.PUBLIC_SECRET or security_config.SECRET
                 )
         except DecodeError as e:
             raise JWTDecodeError(str(e))
