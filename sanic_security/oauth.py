c1d214455739b8f95c24e4d83fd6c811173e65a5
na-stewart <na.stewart365@gmail.com>
2025-01-05 19:22:20 -0500

    Untested oauth implementation

--- sanic_security/oauth.py
+++ sanic_security/oauth.py
@@ -21,34 +21,36 @@ SOFTWARE.
 """
 
 import datetime
+from difflib import get_close_matches
 
 import jwt
 from httpx_oauth.oauth2 import BaseOAuth2, OAuth2Token
 from sanic import Sanic, Request, HTTPResponse
 
 from sanic_security.configuration import config
+from sanic_security.utils import is_expired
 
-clients: [BaseOAuth2]
+clients: dict[str, BaseOAuth2]
 
 
-async def oauth(redirect_uri, *scope: str):
-    authorization_url = await get_client().get_authorization_url(
-        redirect_uri
-        scope=scope
+async def oauth(client: str, scopes: list[str], **kwargs) -> str:
+    authorization_url = await get_client(client).get_authorization_url(
+        config.OAUTH_REDIRECT, scope=scopes, extras_params=kwargs
     )
     return authorization_url
 
 
-async def oauth_callback(request: Request) -> HTTPResponse:
-    token_info = await get_client().get_access_token(
+async def oauth_callback(request: Request, client: str) -> dict:
+    token_info = await get_client(client).get_access_token(
         request.args.get("code"),
         "https://www.vitapath.io/api/v1/security/oauth/callback",
     )
+    return token_info
 
 
-def encode_token_info(response: HTTPResponse, token_info: OAuth2Token, client: str):
+def encode(response: HTTPResponse, token_info: OAuth2Token) -> None:
     response.cookies.add_cookie(
-        f"{config.SESSION_PREFIX}_{client.lower()[:7]}",
+        f"{config.SESSION_PREFIX}_oauth",
         str(
             jwt.encode(
                 token_info, config.SECRET, algorithm=config.SESSION_ENCODING_ALGORITHM
@@ -59,52 +61,37 @@ def encode_token_info(response: HTTPResponse, token_info: OAuth2Token, client: s
         secure=config.SESSION_SECURE,
         domain=config.SESSION_DOMAIN,
         expires=token_info["expires_at"]
-                + datetime.timedelta(seconds=config.AUTHENTICATION_REFRESH_EXPIRATION),
+        + datetime.timedelta(seconds=config.AUTHENTICATION_REFRESH_EXPIRATION),
     )
-    return response
 
 
-def decode_token_info(request: Request, token_info: OAuth2Token, client: str):
-    request.ctx.o_auth = jwt.decode(
+def decode(request: Request) -> dict:
+    token_info = jwt.decode(
         request.cookies.get(
-            f"{config.SESSION_PREFIX}_{client.lower()[:7]}",
+            f"{config.SESSION_PREFIX}_oauth",
         ),
         config.PUBLIC_SECRET or config.SECRET,
         config.SESSION_ENCODING_ALGORITHM,
     )
+    request.ctx.oauth = token_info
+    return token_info
 
 
-def get_client() -> BaseOAuth2:
-    pass
+async def refresh(request: Request, client: str) -> None:
+    token_info = decode(request)
+    if is_expired(token_info["expires_at"]):
+        request.ctx.oauth = await get_client(client).refresh_token(
+            request.ctx.o_auth["refresh_token"]
+        )
+        request.ctx.o_auth["is_refresh"] = True
+
+
+def get_client(client: str) -> BaseOAuth2:
+    closest_match = get_close_matches(client, clients.keys(), n=1)
+    return clients[closest_match[0]] if closest_match else None
 
 
 def initialize_oauth(app: Sanic, *oauth_clients: BaseOAuth2) -> None:
     global clients
-    clients = oauth_clients
-
-    @app.on_request
-    async def token_acquisition_middleware(request):
-        if request.cookies.get("oauth_fitbit"):
-            request.ctx.o_auth = jwt.decode(
-                request.cookies.get("oauth_fitbit"),
-                security_config.SECRET,
-                algorithms=["HS256"],
-            )
-            if time.time() > request.ctx.o_auth["expires_at"]:
-                request.ctx.o_auth = await o_auth.refresh_token(
-                    request.ctx.o_auth["refresh_token"]
-                )
-                request.ctx.o_auth["is_refresh"] = True
-
-    @app.on_response
-    async def refresh_encoder_middleware(request, response):
-        if hasattr(request.ctx, "oauth") and getattr(
-                request.ctx.oauth, "is_refresh", False
-        ):
-            response.cookies.add_cookie(
-                "oauth_fitbit",
-                jwt.encode(
-                    request.ctx.o_auth, security_config.SECRET, algorithm="HS256"
-                ),
-                httponly=True,
-            )
+    for client in oauth_clients:
+        clients[client.__class__.__name__.lower()] = client
