5ca7c49d6bd92d84b14d2ee41d8bd489692a9f85
na-stewart <na.stewart365@gmail.com>
2025-01-08 00:06:08 -0500

    OAuth docs

--- sanic_security/oauth.py
+++ sanic_security/oauth.py
@@ -6,7 +6,7 @@ import jwt
 from httpx_oauth.oauth2 import BaseOAuth2, RefreshTokenError
 from jwt import DecodeError
 from sanic import Request, HTTPResponse, redirect, Sanic
-from tortoise.exceptions import IntegrityError
+from tortoise.exceptions import IntegrityError, DoesNotExist
 
 from sanic_security.configuration import config
 from sanic_security.exceptions import JWTDecodeError, ExpiredError, CredentialsError
@@ -48,7 +48,7 @@ async def oauth_redirect(
     Constructs the authorization URL and returns a redirect response to prompt the user to authorize the application.
 
     Args:
-        client: oauth client
+        client (BaseOAuth2): OAuth provider.
         redirect_uri (str): The URL where the user will be redirected after authorization.
         scope (list[str]): The scopes to be requested. If not provided, `base_scopes` will be used.
         state (str): An opaque value used by the client to maintain state between the request and the callback.
@@ -77,6 +77,24 @@ async def oauth_callback(
     redirect_uri: str = config.OAUTH_REDIRECT,
     code_verifier: str = None,
 ) -> tuple[dict, AuthenticationSession]:
+    """
+    Requests an access token using the authorization code obtained after the user has authorized the application.
+    An account is retrieved if it already exists, created if it doesn't, and the user is logged in.
+
+    Args:
+        request (Request): Sanic request parameter.
+        client (BaseOAuth2): OAuth provider.
+        redirect_uri (str): The URL where the user was redirected after authorization.
+        code_verifier (str): Optional code verifier used in the [PKCE](https://datatracker.ietf.org/doc/html/rfc7636)) flow.
+
+    Raises:
+        CredentialsError
+        GetAccessTokenError
+        GetIdEmailError
+
+    Returns:
+        oauth_redirect
+    """
     access_token = await client.get_access_token(
         request.args.get("code"),
         redirect_uri,
@@ -93,17 +111,31 @@ async def oauth_callback(
         )
         return access_token, authentication_session
     except IntegrityError:
-        raise CredentialsError(f"An account with this email may already exist.", 409)
+        raise CredentialsError(
+            f"Account may not be linked to an OAuth provider if it already exists.", 409
+        )
+    except DoesNotExist:
+        raise CredentialsError(
+            "Account may already be linked to an OAuth provider.", 409
+        )
 
 
 def oauth_encode(
-    response: HTTPResponse, client: Union[BaseOAuth2, str], token_info: dict
+    response: HTTPResponse, client: Union[BaseOAuth2, str], access_token: dict
 ) -> None:
+    """
+    Transforms OAuth access token into JWT and then is stored in a cookie.
+
+    Args:
+        response (HTTPResponse): Sanic response used to store JWT into a cookie on the client.
+        client (Union[BaseOAuth2, str]): OAuth provider.
+        access_token (dict): OAuth access token.
+    """
     response.cookies.add_cookie(
         f"{config.SESSION_PREFIX}_{(client if isinstance(client, str) else client.__class__.__name__)[:7].lower()}",
         str(
             jwt.encode(
-                token_info,
+                access_token,
                 config.SECRET,
                 config.SESSION_ENCODING_ALGORITHM,
             ),
@@ -112,27 +144,44 @@ def oauth_encode(
         samesite=config.SESSION_SAMESITE,
         secure=config.SESSION_SECURE,
         domain=config.SESSION_DOMAIN,
-        max_age=token_info["expires_in"] + config.AUTHENTICATION_REFRESH_EXPIRATION,
+        max_age=access_token["expires_in"] + config.AUTHENTICATION_REFRESH_EXPIRATION,
     )
 
 
 async def oauth_decode(request: Request, client: BaseOAuth2, refresh=False) -> dict:
+    """
+    Decodes OAuth JWT token from client cookie into an access token.
+
+    Args:
+        request (Request): Sanic request parameter.
+        client (BaseOAuth2): OAuth provider.
+        refresh (bool): Ensures that the decoded access token is refreshed.
+
+    Raises:
+        JWTDecodeError
+        ExpiredError
+        RefreshTokenNotSupportedError
+
+    Returns:
+        access_token
+
+    """
     try:
-        access = jwt.decode(
+        access_token = jwt.decode(
             request.cookies.get(
                 f"{config.SESSION_PREFIX}_{client.__class__.__name__[:7].lower()}",
             ),
             config.PUBLIC_SECRET or config.SECRET,
             config.SESSION_ENCODING_ALGORITHM,
         )
-        if time.time() > access["expires_at"] or refresh:
-            access = await client.refresh_token(access["refresh_token"])
-            access["is_refresh"] = True
-            access["client"] = client.__class__.__name__
-            if "expires_at" not in access:
-                access["expires_at"] = time.time() + access["expires_in"]
-        request.ctx.oauth_redirect = access
-        return access
+        if time.time() > access_token["expires_at"] or refresh:
+            access_token = await client.refresh_token(access_token["refresh_token"])
+            access_token["is_refresh"] = True
+            access_token["client"] = client.__class__.__name__
+            if "expires_at" not in access_token:
+                access_token["expires_at"] = time.time() + access_token["expires_in"]
+        request.ctx.oauth = access_token
+        return access_token
     except RefreshTokenError:
         raise ExpiredError
     except DecodeError:
@@ -140,6 +189,26 @@ async def oauth_decode(request: Request, client: BaseOAuth2, refresh=False) -> d
 
 
 def requires_oauth(client: BaseOAuth2):
+    """
+    Decodes OAuth JWT token from client cookie into an access token.
+
+    Args:
+        client (BaseOAuth2): OAuth provider.
+
+    Example:
+        This method is not called directly and instead used as a decorator:
+
+            @app.post('api/oauth')
+            @requires_oauth
+            async def on_oauth(request):
+                return text('OAuth access token retrieved!')
+
+    Raises:
+        JWTDecodeError
+        ExpiredError
+        RefreshTokenNotSupportedError
+    """
+
     def decorator(func):
         @functools.wraps(func)
         async def wrapper(request, *args, **kwargs):
@@ -152,11 +221,20 @@ def requires_oauth(client: BaseOAuth2):
 
 
 def initialize_oauth(app: Sanic) -> None:
+    """
+    Attaches refresh encoder middleware.
+
+    Args:
+        app (Sanic): Sanic application instance.
+    """
+
     @app.on_response
     async def refresh_encoder_middleware(request, response):
         if hasattr(request.ctx, "oauth") and getattr(
-            request.ctx.oauth_redirect, "is_refresh", False
+            request.ctx.oauth, "is_refresh", False
         ):
             oauth_encode(
-                response, request.ctx.oauth_redirect["client"], request.ctx.oauth_redirect
+                response,
+                request.ctx.oauth["client"],
+                request.ctx.oauth,
             )
